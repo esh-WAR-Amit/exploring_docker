fe2f891df1ea5b8c37d92b2cfe6158069e796484
Amiteshwar <amiteshwar551@gmail.com>
2025-04-18 13:50:58 +0530

    backend containerization

--- SIH_backend/config/db.js
+++ SIH_backend/config/db.js
@@ -1,13 +1,29 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Connected to Mongodb Database ${conn.connection.host}`);
-    }
-    catch(err){
-        console.log (`Error in Mongodb ${err}`);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Connected to Mongodb Database ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`Error in Mongodb ${err}`);
+  }
+};
 
-export default connectDB;
+// export default connectDB;
+//************************************************************************** */
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB connected to:", conn.connection.host);
+//     console.log("Base MongoDB URL:", process.env.MONGO_URI);
+//   } catch (err) {
+//     console.error("MongoDB connection error:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
